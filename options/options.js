9878416bf666326c1061f45ef6800bd3cfc7e2d5
Crowdbotics <engineering@crowdbotics.com>
2023-03-20 07:23:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gtime-18",
+  url: "https://gtime-18.botics.co",
+  api: "https://gtime-18.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
